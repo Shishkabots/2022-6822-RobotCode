02de82b13d1adf2bc720d97c913f4b9fa45c2b47
Unreal Champaign <88566051+unrealchampaign@users.noreply.github.com>
2021-10-24 21:27:02 -0700

    Add initial unit test file for arcade drive

---
+++ src/main/test/frc/robot/commands/ArcadeDriveTest.java
@@ -0,0 +1,18 @@
+package frc.robot.commands;
+
+import org.junit.Test;
+
+import jdk.internal.jline.internal.TestAccessible;
+
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+
+public class ArcadeDriveTest {
+   
+    @Test
+    public void testBaselineInitialization() {
+        System.out.println("Hello");
+        return;
+    }
+}
