d20590e03e1bdf9537d630a54437c85f67c51f8f
Liliane Wu <wuyanxi1118@gmail.com>
2021-10-30 14:35:20 -0700

    test2

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -44,7 +44,7 @@ public class ArcadeDrive extends CommandBase {
   @Override
   public void execute() {
     m_drivetrain.arcadedrive(m_speed.getAsDouble(), m_rotation.getAsDouble());
-    if(m_driverStick.getY() == 0 && m_driverStick.getX() == 0){
+    if(m_driverStick.getY() == 0){
       smoothStop(m_speed.getAsDouble());
     }
   }
@@ -60,15 +60,18 @@ public class ArcadeDrive extends CommandBase {
   public boolean isFinished() {
     return false; // Runs until interrupted
   }
-  
+
   public void smoothStop(double speedLoc){
+    
     double curSpeed = speedLoc;
+    System.out.println("Smoothe stop called, curSpeed: " + curSpeed);
     if(curSpeed == 0){
       return;
     } else {
+      
       curSpeed /= 2;
       m_drivetrain.arcadedrive(curSpeed, m_rotation.getAsDouble());
-      smoothStop(speedLoc);
+      smoothStop(curSpeed);
     }
   }
 }
