baf4eecd79fa5985202f8872d1a69266d128e2fc
Liliane Wu <wuyanxi1118@gmail.com>
2021-11-13 15:40:47 -0800

    change arcadeD

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -15,18 +15,21 @@ public class ArcadeDrive extends CommandBase {
   private final DriveTrain m_drivetrain;
   private final DoubleSupplier m_speed;
   private final DoubleSupplier m_rotation;
+  private boolean m_stop;
+  private double curSpeed;
 
   /**
    * Creates a new ArcadeDrive command.
    *
-   * @param  drivetrain The drivetrain used by this command.
+   * @param drivetrain The drivetrain used by this command.
    */
-  public ArcadeDrive(DoubleSupplier speed, DoubleSupplier rotation, DriveTrain drivetrain, double sensitivity) {
+  public ArcadeDrive(DoubleSupplier speed, DoubleSupplier rotation, DriveTrain drivetrain, double sensitivity, boolean Stop) {
     m_drivetrain = drivetrain;
     m_speed = () -> speed.getAsDouble() * sensitivity;
     m_rotation = () -> rotation.getAsDouble() * sensitivity;
+    curSpeed = m_speed.getAsDouble();
+    m_stop = Stop;
     addRequirements(m_drivetrain);
-
   }
 
   // Called when the command is initially scheduled.
@@ -40,12 +43,15 @@ public class ArcadeDrive extends CommandBase {
   @Override
   public void execute() {
     m_drivetrain.arcadedrive(m_speed.getAsDouble(), m_rotation.getAsDouble());
+    if(m_stop){
+      smoothStop(m_speed.getAsDouble());
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    System.out.println("Arcadedrive ended, interrupted = " + interrupted);
+    System.out.println("Arcadedrive ended, interreupted = " + interrupted);
   }
 
   // Returns true when the command should end.
@@ -53,5 +59,23 @@ public class ArcadeDrive extends CommandBase {
   public boolean isFinished() {
     return false; // Runs until interrupted
   
-  }  
+  }
+  public void smoothStop(double speedLoc){
+    curSpeed = speedLoc;
+    if(curSpeed < 1){
+      curSpeed = 0;
+      m_drivetrain.arcadedrive(curSpeed, m_rotation.getAsDouble());
+      System.out.println("speed = 0");
+      return;
+    } else {
+      curSpeed /= 2;
+      System.out.println("speed = " + curSpeed);
+      m_drivetrain.arcadedrive(curSpeed, m_rotation.getAsDouble());
+      System.out.println("speed updated");
+      smoothStop(curSpeed);
+    }
+  }
+  public double getCurrentSpeed(){
+    return curSpeed;
+  }
 }
